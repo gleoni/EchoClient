97fc6cfc3d4dcda6fa5dcea3fc85ed08352a9a12
LEONI.GIUSEPPE <LEONI.GIUSEPPE@Cons-ITI-Diurno.OSDBSESTO.local>
2014-02-28 10:44:34 +0100

    Prima versione del lavoro

---
+++ src/echoclient/EchoClient.java
@@ -0,0 +1,22 @@
+package echoclient;
+
+import java.io.*;
+import java.net.*;
+
+public class EchoClient {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket s = new Socket("127.0.0.1", 5555);
+        BufferedReader sock_in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        PrintWriter sock_out = new PrintWriter(new OutputStreamWriter(s.getOutputStream()), true);
+        BufferedReader std_in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter std_out = new PrintWriter(new OutputStreamWriter(System.out), true);
+        
+        while (true) {
+            sock_out.println(std_in.readLine());
+            //std_out.println(sock_in.readLine());
+            System.out.println(sock_in.readLine());
+        }
+    }
+}
